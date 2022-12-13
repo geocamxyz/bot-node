3daf4f96b6a18e79f63ce15b6da2cc9a0ab28f9e
Julian Cox <julian.cox@webspeed.co.nz>
2022-12-14 11:57:26 +1300

    moved conductor node to bot node

---
+++ bot.js
@@ -0,0 +1,97 @@
+module.exports = function (RED) {
+  const os = require("os");
+  const ZB = require("zeebe-node");
+  const requestTimeout = 5000;
+
+  function bot(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const globals = node.context().global;
+    const task = config.name;
+    const hostname = os.hostname;
+    const active = {};
+
+    const zeebeClient = function () {
+      let client = globals.get("zbc");
+      if (!client) {
+        globals.set("availableCompute", 1);
+        client = new ZB.ZBClient("zeebe.geocam.xyz:26500", {
+          onReady: () => {
+            console.log("zeebe client connected");
+          },
+        });
+        globals.set("zbc", client);
+      }
+      return client;
+    };
+
+    const poll = async function (capability) {
+      const running = Object.keys(active).length;
+      const slots = capability.limit - running;
+      const numJobs = Math.min(
+        Math.floor(globals.get("availableCompute") / capability.compute),
+        slots
+      );
+      const zbc = zeebeClient();
+      req = {
+        maxJobsToActivate: slots,
+        requestTimeout: requestTimeout,
+        timeout: capability.timeout,
+        type: task,
+        worker: hostname,
+      };
+      jobs = await zbc.activateJobs(req);
+      jobs.forEach((job) => {
+        active[job.key] = job;
+        globals.set(
+          "availableCompute",
+          globals.get("availableCompute") - capability.compute
+        );
+        const done = function (errorMessage = null) {
+          delete active[job.key];
+          globals.set(
+            "availableCompute",
+            globals.get("availableCompute") + capability.compute
+          );
+          errorMessage ? zbc.failJob({ jobKey: job.key, errorMessage: errorMessage}) : zbc.completeJob({ jobKey: job.key, variables: job.variables });
+        };
+        msg = { payload: { job: job, done: done } };
+        node.send(msg);
+      });
+    };
+
+    node.on("input", function (msg) {
+      // find my name in array
+      const capabilities = msg.payload;
+      let stored = globals.get('capabilities');
+      console.log('got stored capabilities',stored)
+      if (!stored) {
+        stored = capabilities.map(c => c.task_type);
+        globals.set('capabilities',stored);
+      }
+      const idx = capabilities.findIndex((c) => c["task_type"] === task);
+      if (idx >= 0) {
+        stored.splice(stored.indexOf(task), 1);
+        globals.set('capabilities', stored);
+        console.log('set stored capabilities',stored)
+        this.status({
+          fill: "green",
+          shape: "dot",
+          text: `Polling for ${task} jobs`,
+        });
+        setTimeout(() => poll(capabilities[idx], idx * requestTimeout));
+      } else {
+        this.status({
+          fill: "yellow",
+          shape: "dot",
+          text: `Not a ${hostname} task`,
+        });
+      }
+      // if it's there wait based on place in array
+      // then request jobs based on max and compute
+      const zbc = zeebeClient();
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("bot", bot);
+};
