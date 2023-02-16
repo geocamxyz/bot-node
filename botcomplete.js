2628075e164d82a5a5dc57477944f265c26aa7b0
Julian Cox <julian.cox@webspeed.co.nz>
2023-02-17 10:18:51 +1300

    trying to figure out why done not called properly on error

--- botcomplete.js
+++ botcomplete.js
@@ -6,16 +6,20 @@ module.exports = function (RED) {
 
     node.on("input", function (msg) {
       const payload = msg.payload && msg.payload.job ? msg.payload : msg.zeebePayload;
+      node.warn( payload)
       if (payload && payload.done) {
         const  error  = payload.job && payload.job.error;
         const variables = payload.job && payload.job.variables;
+        node.warn(`about to call done with error ${error}`);
         payload.done(error,variables,node).catch((err) => {
+          node.warn(`got errror calling payload done ${err}`);
           node.status({
             fill: "red",
             shape: "ring",
             text: `zeebe err: ${err}`,
           });
         });
+        node.warn('payload done called')
       }
       if (payload.job && payload.job.error) {
         node.status({
